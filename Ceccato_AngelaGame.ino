e1499b2b28cbe89cf6f807f03213585c01566951
Francesco Ceccato <francesco.ceccato@barsanti.edu.it>
2019-01-23 16:58:06 +0100

    23/01/2019 - Build 1, assegnazione meta funzionante

---
+++ Ceccato_AngelaGame.ino
@@ -0,0 +1,42 @@
+int turn = 0;
+bool zero = false;
+int meta = 5;
+int current = 0;
+int previousMove;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+      setMeta();
+
+      while(true);
+}
+
+int setMeta()
+{
+  getMeta();
+  printMeta();
+}
+
+int getMeta()
+{
+  int i;
+  bool done = false;
+  while(!done)
+  {      
+      Serial.println("Digita un numero compreso fra 1 e 99!");
+      while (Serial.available() == 0);
+      i = Serial.parseInt();
+      done = (i>0 && i<100) ? true : false;
+  }
+  meta = i;
+}
+
+int printMeta()
+{
+      String s = "La meta scelta ammonta a " + meta; 
+      Serial.println(s);
+}
+
