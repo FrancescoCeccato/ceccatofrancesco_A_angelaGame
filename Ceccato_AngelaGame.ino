25a26437337abbb015b932273760e99c3a889dae
Francesco Ceccato <francesco.ceccato@barsanti.edu.it>
2019-01-24 17:54:42 +0100

    24/01/2019 - Build 2: completo

--- Ceccato_AngelaGame.ino
+++ Ceccato_AngelaGame.ino
@@ -1,19 +1,21 @@
 bool turn = true;
 bool zero = true;
 int meta = 0;
-int current = -1;
+int current = 0;
 int previous;
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("Benvenuti in Angela Game!");
+  Serial.println("Un gioco di P.Perotto, porting by F.Ceccato");
 }
 
 void loop() {
    setMeta();
-   while(current<meta)
-   {
+   while(current<meta){
     execTurn();
    }
+   gameOver();
 }
 
 int setMeta(){
@@ -25,9 +27,9 @@ void getMeta(){
   int i;
   bool done = false;
   while(!done){      
-      Serial.println("- Digita un numero intero compreso fra 1 e 99!");
+      Serial.println("- Si digiti un numero intero compreso fra 30 e 99!");
       while (Serial.available() == 0);
-      i = Serial.parseInt();
+      Serial.println(String(i = Serial.parseInt()));
       done = (i>=30 && i<100);
   }
   meta = i;
@@ -41,10 +43,10 @@ void printMeta(){
 void execTurn(){
   int i;
   bool done = false;
-  Serial.println("- Turno di P"  + String(turn? "1" : "2") + " - Punti: " + String(current)+"/"+String(meta));
+  Serial.println("- Turno di P"  + String(turn? "1" : "2") + " - Punti: " + counter());
   while(!done){  
     while (Serial.available() == 0);
-    i = Serial.parseInt();
+    Serial.println(String(i = Serial.parseInt()));
     done = evaluateTurn(i);
   } 
   previous = i;
@@ -53,15 +55,53 @@ void execTurn(){
 }
 
 bool evaluateTurn(int i){
-  if(i == previous || i == 7-previous || (i==0 && !zero)){ 
+  if(i==0 && zero){
+    return true;
+  }
+  else if(i>6 || i<0){
+    Serial.println("- Inserisci un numero valido!");
+    return false;
+  }
+  else if((i == 0 && !zero)|| i == previous || i == 7-previous){ 
     Serial.println("Non barare!");
     return false;
   }
-  else if(i == 0 && zero){
+  else
+  {
     zero = false;
     return true;
   }
-  else if(i>6)
-  return false;
-  else return true;
+}
+
+void gameOver()
+{
+  Serial.println("Il vincitore è " + String((turn == (current>meta)) ? "P1" : "P2") + "! " + counter());
+  resetValues();
+  Serial.println("Premi [Invia] per rigiocare!");
+  while (Serial.available() == 0);
+
+  /*La condizione "bool == bool" equivale all'operatore logico A XNOR B.
+   *TURN(A)|current>meta(B)| A XNOR B  | WINNER
+   *TRUE   |     TRUE      |   TRUE    |   P1
+   *TRUE   |     FALSE     |   FALSE   |   P2
+   *FALSE  |     TRUE      |   FALSE   |   P2
+   *FALSE  |     FALSE     |   TRUE    |   P1
+   *
+   *Si osservi la tavola della verità. Il valore di TURN è quello del giocatore che normalmente inizierebbe il turno:
+   *dovesse P1 iniziare il turno con current > di meta, allora P2 ha perduto, e viceversa.
+   *Questo metodo di valutazione si interpreta con fatica ad occhio, ma risparmia delle righe di codice.
+   */
+}
+
+void resetValues()
+{
+  turn = true;
+  zero = true;
+  meta = 0;
+  current = 0;
+  previous = 0;
+}
+
+String counter(){
+  return String(current)+"/"+String(meta);
 }
